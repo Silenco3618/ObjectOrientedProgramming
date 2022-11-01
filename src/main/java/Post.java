e47da2de44196a3f8785a99315f3a3e8a2234ae8
Julia Silenco <silenko10031986@gmail.com>
2022-11-01 17:20:50 +0400

    all add

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;//день рождения
+    public String name; // поле имени
+    public String passport;// поле паспорта
+    public String patronymic;// поле отчества
+    public String phone;// поле телефона
+    public String surname;// поле фамилии
+    public Boolean subscription;
+}
